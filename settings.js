b613d0cd0d0762feab4f64beb16faaf5ced8baf6
nserekonajib <109471084+nserekonajib@users.noreply.github.com>
2026-01-04 22:36:12 +0300

    Remove '+' symbol from ownerNumber

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Lunserk Technologies', // Your name
-  ownerNumber: '+256748675870', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '256748675870', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
